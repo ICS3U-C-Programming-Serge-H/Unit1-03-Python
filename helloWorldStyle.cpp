ad4dbe167979802ed7d570e2f6353d7f0bcb3c66
Serge Hamouche <Serge.el-hamouche@stu.ocsb.ca>
2025-02-15 21:07:52 +0000

    helloWorldStyle.cpp

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2021 Ms Raffin All rights reserved.
+// .
+// Created by: Ms Raffin
+// Date: Apr. 22, 2021
+// This program displays "Hello, World with style".
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World with style!" << std::endl;
+    std::cout << "C++ is a bit more complicated then Python!" << std::endl;
+}
